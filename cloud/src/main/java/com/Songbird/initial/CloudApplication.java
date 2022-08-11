5c664b4e264ba0ec1dfb3c11c8a924d71fe91760
qin <158649449@qq.com>
2022-08-11 16:30:10 +0800

    修改成springcloud

---
+++ cloud/src/main/java/com/Songbird/initial/CloudApplication.java
@@ -0,0 +1,15 @@
+package com.Songbird.initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+        System.out.println("启动成功～ 0。0");
+
+    }
+
+}
---
+++ cloud/src/main/java/com/Songbird/initial/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.Songbird.initial.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/get/name")
+    public String getName(String name){
+        return "name:"+name;
+    }
+}
